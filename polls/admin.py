b297fc6695f0c4b77d7eef668973ec641fa2dfb8
v0kan <axineop@gmail.com>
2021-01-23 14:52:00 +0100

    Changed admin panel, updated models

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from polls.models import Category, Page
 
-admin.site.register(Category)
-admin.site.register(Page)
+
+class PageInLine(admin.StackedInline):
+    model = Page
+    extra = 0
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        ('Stats:', {'fields': ['views', 'likes']}),
+    ]
+    inlines = [PageInLine]
+
+    list_display = ('name', "views", "likes")
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(Page, PageAdmin)
