9ef6e239423aecbc7162a0b8c1ee21924fceb0bd
v0kan <axineop@gmail.com>
2021-01-30 11:22:45 +0100

    Added server-side cookies

--- polls/views.py
+++ polls/views.py
@@ -9,6 +9,8 @@ from django.urls import reverse
 
 from django.contrib.auth.decorators import login_required
 
+from datetime import datetime
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -19,13 +21,15 @@ def index(request):
         'pages': page_list,
     }
 
-    return render(request, 'polls/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+
+    context_dict['visits'] = request.session['visits']
+
+    return render(request, 'polls/index.html', context_dict)
 
 
 def about(request):
-    print(request.method)
-    print(request.user)
-    return render(request, 'polls/about.html', context={})
+    return render(request, 'polls/about.html')
 
 
 def show_category(request, category_name_slug):
@@ -52,7 +56,7 @@ def add_category(request):
         form = CategoryForm(request.POST)
 
         if form.is_valid():
-            form.save(commit=True)
+            form.save()
             return index(request)
         else:
             print(form.errors)
@@ -142,10 +146,33 @@ def user_login(request):
         else:
             print("Invalid login details: {0}, {1}".format(username, password))
             return HttpResponse("Invalid login details supplied.")
-    else:
-        return render(request, 'polls/login.html')
+    return render(request, 'polls/login.html')
 
 
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits += 1
+
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
